8ebc5fb52a8e0af00b9ebc407fe7bbc700bd6273
jamil <jamilhossain3251@gmail.com>
2023-05-17 23:05:10 +0600

    navbar

--- src/body/App.jsx
+++ src/body/App.jsx
@@ -6,9 +6,17 @@ function App() {
   return (
     <>
       
-      <p className="read-the-docs">
-        Click on the Vite and React logos to 
-      </p>
+      <div className="hero min-h-screen bg-[#232e36]">
+  <div className="hero-content flex-col lg:flex-row-reverse text-white">
+    <img src="https://i.ibb.co/F6GpjQy/istockphoto-173948450-612x612-removebg-preview.png" className="max-w-[50%]" />
+    <div>
+      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+
     </>
   )
 }
--- src/body/Navbar/Navbar.jsx
+++ src/body/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar(){
 
       <div>
 
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 absolute top-0 bg-transparent text-white">
           <div className="navbar-start">
             <div className="dropdown">
               <label tabIndex={0} className="btn btn-ghost lg:hidden">
