8e4f04b452f01ef454f8ac408f5305c7816c257a
jamil <jamilhossain3251@gmail.com>
2023-05-20 17:00:09 +0600

    update

--- src/authentication/firebase/firebase_config.js
+++ src/authentication/firebase/firebase_config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
-  measurementId: import.meta.env.VITE_MEASUREMENTID
+  apiKey: "AIzaSyAoOrbh9QVsgHF7KwJjxRt-ig7hRKaeFAs",
+  authDomain: "assignment-11-68854.firebaseapp.com",
+  projectId: "assignment-11-68854",
+  storageBucket: "assignment-11-68854.appspot.com",
+  messagingSenderId: "327260469304",
+  appId: "1:327260469304:web:7880dffed14701fdc68ecd",
+  measurementId: "G-MWMNEZRQTF"
 };
 
 
